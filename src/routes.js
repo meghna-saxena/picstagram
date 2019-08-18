22cfffdfdd671b6c1bbc4cfe590a5d60b5658158
Meghna Srivastava <meghna.s1906@gmail.com>
2019-08-18 21:10:41 +0200

    removed extra wrapper router component

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
 import * as React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
 import SinglePhoto from "./components/SinglePhoto";
 
 export const routes = (
-  <Router>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/view/:postId" component={SinglePhoto} />
       {/* <Route component={Notfound} /> */}
     </Switch>
-  </Router>
 );
