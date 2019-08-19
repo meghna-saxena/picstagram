66f8dfff3d0374ebd6000507fe31738f6b241914
Meghna Srivastava <meghna.s1906@gmail.com>
2019-08-19 10:32:09 +0200

    addded all styles in index file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 
-import PhotoGrid from './PhotoGrid'
+import PhotoGrid from "./PhotoGrid";
+
+import styles from "../styles/main.module.styl";
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.heading}>
         <h1>
           <Link to="/">Picstagram</Link>
         </h1>
